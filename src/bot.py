fbf5ecb8a10e6de47dc4f89000bb41d987e28297
jakemrh <jacobhoernlein@gmail.com>
2022-07-10 14:06:34 -0400

    Added manual control.

--- src/bot.py
+++ src/bot.py
@@ -1,44 +1,44 @@
 import os
 import asyncio
-import threading
 
 import discord
-from cleverbotfreeapi import cleverbot
 
-crag = discord.Client()
 
-def auto():
-    @crag.event
-    async def on_message(msg:discord.Message):
-        if msg.channel.id == 994482057336074341 and msg.author.id != 994430080275206184:
-            response = cleverbot(msg.content, session="cragbot2022")
-            await msg.channel.send(response)
-        
-    @crag.event
-    async def on_ready():
+class CragBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.channel = None
+
+    async def on_ready(self):
+        self.channel = await self.fetch_channel('994482057336074341')
         print("crag.")
 
-    crag.run(os.getenv('BOTTOKEN'))
-
-async def manual():
-    await crag.login(os.getenv('BOTTOKEN'))
-    channel = await crag.fetch_channel('994482057336074341')
-    
-    while True:
-        try:
-            message = input("> ")
-        except KeyboardInterrupt:
-            await crag.close()
-            exit()
-        else:
-            await channel.send(message)
 
 if __name__ == '__main__':
-    mode = input("Select mode. 1 = Auto, 2 = Manual: ")
+    crag = CragBot(intents=discord.Intents.all())
+    
+    mode = input("Select mode. 1) Auto, 2) Manual: ")
 
+    # Auto Mode. Uses Cleverbot API.
     if int(mode) == 1:
-        auto()
+        from cleverbot import async_ as cleverbot
+        cb = cleverbot.load('cleverbot.crag')
+        
+        @crag.event
+        async def on_message(msg:discord.Message):
+            if str(msg.channel.id) == '994482057336074341' \
+            and str(msg.author.id) != '994430080275206184':
+                response = await cb.say(msg.content)
+                cb.save('cleverbot.crag')
+                await msg.channel.send(response)
+
+    # Manual Mode. Uses Jacob's DMs.
     elif int(mode) == 2:
-        asyncio.run(manual())
+        @crag.event
+        async def on_message(msg:discord.Message):
+            if msg.channel.type == discord.ChannelType.private \
+            and str(msg.author.id) == '243845903146811393':
+                await crag.channel.send(msg.content)
 
-
+    crag.run(os.getenv('BOTTOKEN'))
+    asyncio.run(cb.close())
