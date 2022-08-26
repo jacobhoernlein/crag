9d8d0f7c1d9b70c7641272a2209863b8c780c99e
jakemrh <jacobhoernlein@gmail.com>
2022-08-26 10:41:50 -0500

    changed to v2

--- src/main.py
+++ src/main.py
@@ -1,63 +1,49 @@
 import os
-import asyncio
-
 import discord
+from discord.ext import commands
+import cleverbot
 
 
-class CragBot(discord.Client):
+class CragBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.channel = None
+        self.channel:discord.TextChannel = None
+        self.mode = 1
+        self.authed_users = ['243845903146811393', '534939215855878151']
+        self.cb = cleverbot.load('cleverbot.crag')
 
     async def on_ready(self):
-        self.channel = await self.fetch_channel('994482057336074341')
+        self.channel = await self.fetch_channel('1010692822128672829')
+        await self.tree.sync()
         print("crag.")
 
-
-async def send_messages(bot:CragBot):
-    await bot.login(os.getenv('BOTTOKEN'))
-    channel = await bot.fetch_channel('994482057336074341')
-
-    while True:
-        try:
-            message = input("> ")
-        except KeyboardInterrupt:
-            await bot.close()
-            break
-        else:
-            await channel.send(message)
-    
-if __name__ == '__main__':
-    crag = CragBot(intents=discord.Intents.all())
-
-    mode = input("Select mode. 1) Cleverbot, 2) DMs, 3) Terminal: ")
-
-    # Auto Mode. Uses Cleverbot API.
-    if int(mode) == 1:
-        from cleverbot import async_ as cleverbot
-        cb = cleverbot.load('cleverbot.crag')
-        
-        @crag.event
-        async def on_message(msg:discord.Message):
-            if str(msg.channel.id) == '994482057336074341' \
-            and str(msg.author.id) != '994430080275206184':
-                response = await cb.say(msg.content)
-                cb.save('cleverbot.crag')
+    async def on_message(self, msg:discord.Message):
+        if self.mode == 1:
+            if msg.channel == self.channel \
+            and msg.author != self.user:
+                response = self.cb.say(msg.content)
+                self.cb.save('cleverbot.crag')
                 await msg.channel.send(response)
-
-        crag.run(os.getenv('BOTTOKEN'))
-        asyncio.run(cb.close())
-
-    # Manual Mode. Uses Jacob's DMs.
-    elif int(mode) == 2:
-        @crag.event
-        async def on_message(msg:discord.Message):
+        
+        if self.mode == 2:
             if msg.channel.type == discord.ChannelType.private \
-            and str(msg.author.id) == '243845903146811393':
+            and str(msg.author.id) in crag.authed_users:
                 await crag.channel.send(msg.content)
 
-        crag.run(os.getenv('BOTTOKEN'))
+crag = CragBot(command_prefix="crag.", intents=discord.Intents.all())
 
-    # Terminal Mode. Does not show bot as Online.
-    elif int(mode) == 3:
-        asyncio.run(send_messages(crag))
+@crag.tree.command(name="mode", description="Does nothing.")
+async def change_mode(interaction:discord.Interaction):
+    if str(interaction.user.id) not in crag.authed_users:
+        await interaction.response.send_message("No.", ephemeral=True)
+        return
+    if crag.mode == 1:
+        crag.mode = 2
+        await interaction.response.send_message("Changed to Manual.", ephemeral=True)
+        return
+    if crag.mode == 2:
+        crag.mode = 1
+        await interaction.response.send_message("Changed to Auto.", ephemeral=True)
+        return
+
+crag.run(os.getenv('CRAGTOKEN'))
