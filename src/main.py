04bff07f8b8f4ee0695de4ac47dc6c68384a9fe8
jakemrh <jacobhoernlein@gmail.com>
2022-09-29 15:53:32 -0500

    changed from cog to bot subclass

--- src/main.py
+++ src/main.py
@@ -6,106 +6,91 @@ Version 2.
 """
 
 import asyncio
-from dataclasses import dataclass
 import os
-import sqlite3
 
+import aiosqlite
 import cleverbot
 import discord
 from discord.ext import commands
 
 
-@dataclass
-class GuildSetting:
-    """Contains settings for each guild that the bot is in."""
+class CragBot(commands.Bot):
+    """Subclassed commands.Bot that includes database and cleverbot api connections."""
 
-    channel: discord.TextChannel
-    convo: cleverbot.cleverbot.Conversation
+    db: aiosqlite.Connection
+    cb: cleverbot.Cleverbot
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-class CommandsCog(commands.Cog):
-    """Cog that contains all the functionality because idk how to make app commands work in a subclassed bot."""
+        self.tree.add_command(
+            discord.app_commands.Command(
+                name="setchannel",
+                description="Change the channel that crag will live in to the current channel.",
+                callback=self.change_channel_callback
+            )
+        )
+        self.tree.add_command(
+            discord.app_commands.Command(
+                name="donate",
+                description="Support the creator (he is broke).",
+                callback=self.donate_command_callback
+            )
+        )
 
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-        self.cb: cleverbot.Cleverbot = cleverbot.load('cleverbot.crag')
-
-        self.db = sqlite3.connect('crag.sqlite')
-        self.guild_dict: dict[int, GuildSetting] = {}
-        
-    @commands.Cog.listener()
     async def on_ready(self):
-        """Creates guildsettings for each record in the table then adds them to the dictionary."""
+        self.db = await aiosqlite.connect('crag.sqlite')
+        self.cb = cleverbot.load('cleverbot.crag')
 
-        cursor = self.db.execute('SELECT * FROM guilds')
-        guild_records = cursor.fetchall()
-        
-        for guild_record in guild_records:
-            
-            guild_id = guild_record[0]
-            channel_id = guild_record[1]
-            
-            guild: discord.Guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
-            channel: discord.TextChannel = discord.utils.find(lambda c: c.id == channel_id, guild.channels)
-            conversation: cleverbot.cleverbot.Conversation = self.cb.conversations[str(guild_id)]
-
-            self.guild_dict[guild_id] = GuildSetting(channel, conversation)
-
-        await self.bot.change_presence(
+        await self.tree.sync()
+        await self.change_presence(
             activity=discord.Activity(name="/setchannel", type=discord.ActivityType.playing)
         )
-        await self.bot.tree.sync()
         print("crag.")
 
-    @commands.Cog.listener()
+    async def on_disconnect(self):
+        await self.db.close()
+
+    async def on_resumed(self):
+        self.db = await aiosqlite.connect('crag.sqlite')
+    
     async def on_message(self, msg: discord.Message):
         """Listens for messages in the channel set for the server then responds to the conversation."""
 
-        if msg.author == self.bot.user:
+        if msg.author == self.user:
             return
 
-        if msg.guild.id in self.guild_dict.keys():
-            guild_setting = self.guild_dict[msg.guild.id]
-            
-            if msg.channel == guild_setting.channel:
+        async with self.db.execute(f'SELECT channel_id FROM guilds WHERE guild_id = {msg.guild.id}') as cursor:
+            record = await cursor.fetchone()
+        
+        if record is None:
+            return
+
+        channel_id: int = record[0]
+        convo = self.cb.conversations[str(msg.guild.id)]
+
+        if msg.channel.id == channel_id:
+            response = convo.say(msg.content)
+            async with msg.channel.typing():
+                await asyncio.sleep(2)
+            await msg.channel.send(response)
+
+            self.cb.save('cleverbot.crag')
                 
-                response = guild_setting.convo.say(msg.content)
-                async with msg.channel.typing():
-                    await asyncio.sleep(2)
-                await msg.channel.send(response)
-
-                self.cb.save('cleverbot.crag')
-
-    @commands.Cog.listener()
-    async def on_guild_remove(self, guild: discord.Guild):
-        """Removes the guild's entry from the database and server dictionary."""
-
-        if guild.id not in self.guild_dict.keys():
-            return
-
-        del self.guild_dict[guild.id]
-        self.db.execute(f'DELETE FROM guilds WHERE guild_id = {guild.id}')
-        self.db.commit()
-    
-    @discord.app_commands.command(name="setchannel", description="Change the channel that crag will live in to the current channel.")
     @discord.app_commands.guild_only()
-    async def change_channel(self, interaction: discord.Interaction):
+    async def change_channel_callback(self, interaction: discord.Interaction):
         """Sets up the bot to work in the channel the command was run in. Creates a record if it does not exist."""
 
-        if interaction.guild_id not in self.guild_dict.keys():
-            conversation = self.cb.conversation(str(interaction.guild_id))
-            self.guild_dict[interaction.guild_id] = GuildSetting(interaction.channel, conversation)
-            self.db.execute(f'INSERT INTO guilds VALUES ({interaction.guild_id}, {interaction.channel_id})')
+        if str(interaction.guild_id) in self.cb.conversations.keys():
+            await self.db.execute(f'UPDATE guilds SET channel_id = {interaction.channel_id} WHERE guild_id = {interaction.guild_id}')
         else:    
-            self.guild_dict[interaction.guild_id].channel = interaction.channel
-            self.db.execute(f'UPDATE guilds SET channel_id = {interaction.channel_id} WHERE guild_id = {interaction.guild_id}')
+            self.cb.conversation(str(interaction.guild_id))
+            await self.db.execute(f'INSERT INTO guilds VALUES ({interaction.guild_id}, {interaction.channel_id})')
         
-        self.db.commit()
-
+        await self.db.commit()
         await interaction.response.send_message("Channel set up.", ephemeral=True)
         
-    @discord.app_commands.command(name="donate", description="Support the creator (he is broke).")
-    async def donate_command(self, interaction: discord.Interaction):
+    async def donate_command_callback(self, interaction: discord.Interaction):
         """Sends a link to the user that lets them donate to me."""        
         
         await interaction.response.send_message("Please consider supporting my creator at https://paypal.me/jhoernlein")
@@ -113,10 +98,9 @@ class CommandsCog(commands.Cog):
 
 if __name__ == '__main__':
     
-    crag = commands.Bot(
+    crag = CragBot(
         command_prefix='NO PREFIX',
         help_command=None,
         intents=discord.Intents.all()
     )
-    asyncio.run(crag.add_cog(CommandsCog(crag)))
     crag.run(os.getenv('CRAGTOKEN'))
