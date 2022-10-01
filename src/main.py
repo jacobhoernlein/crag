c21de4cece6d941b3a344a2d3c4663b845dfab7c
jakemrh <jacobhoernlein@gmail.com>
2022-09-30 20:24:38 -0500

    create database, handle no save file

--- src/main.py
+++ src/main.py
@@ -40,15 +40,13 @@ class CragBot(commands.Bot):
 
     async def on_ready(self):
         self.db = await aiosqlite.connect('crag.sqlite')
-        await self.db.execute(
-            'CREATE TABLE IF NOT EXISTS guilds (guild_id INTEGER, channel_id INTEGER)'
-        )
+        await self.db.execute('CREATE TABLE IF NOT EXISTS guilds (guild_id INTEGER, channel_id INTEGER)')
         await self.db.commit()
 
         try:
             self.cb = cleverbot.load('crag.cleverbot')
-        except:
-            print("No cleverbot save file found. Create a new one manually and save as crag.cleverbot")
+        except FileNotFoundError:
+            print("No cleverbot save file found. Create a new one manually and save as 'crag.cleverbot'")
             print("Visit https://pypi.org/project/cleverbot.py/ to learn how.")
             await self.close()
 
@@ -73,10 +71,9 @@ class CragBot(commands.Bot):
             return
             
         if record[0] == msg.channel.id:
-
             convo = self.cb.conversations[str(msg.guild.id)]
-            response = convo.say(msg.content)
 
+            response = convo.say(msg.content)
             async with msg.channel.typing():
                 await asyncio.sleep(2)
             await msg.channel.send(response)
@@ -87,14 +84,25 @@ class CragBot(commands.Bot):
     async def change_channel_callback(self, interaction: discord.Interaction):
         """Sets up the bot to work in the channel the command was run in. Creates a record if it does not exist."""
 
-        if str(interaction.guild_id) in self.cb.conversations.keys():
-            await self.db.execute(f'UPDATE guilds SET channel_id = {interaction.channel_id} WHERE guild_id = {interaction.guild_id}')
-        else:    
-            self.cb.conversation(str(interaction.guild_id))
+        async with self.db.execute(f'SELECT * FROM guilds WHERE guild_id = {interaction.guild_id}') as cursor:
+            record = await cursor.fetchone()
+
+        if record is None:
             await self.db.execute(f'INSERT INTO guilds VALUES ({interaction.guild_id}, {interaction.channel_id})')
-        
+        else:
+            await self.db.execute(f'UPDATE guilds SET channel_id = {interaction.channel_id} WHERE guild_id = {interaction.guild_id}')
         await self.db.commit()
-        await interaction.response.send_message("Channel set up.", ephemeral=True)
+
+        key = str(interaction.guild.id)
+        if key not in self.cb.conversations.keys():
+            convo = self.cb.conversation(key)
+        else:
+            convo = self.cb.conversations[key]
+
+        response = convo.say('Tell me something interesting.')
+        await interaction.response.send_message(response)
+
+        self.cb.save('crag.cleverbot')
         
     async def donate_command_callback(self, interaction: discord.Interaction):
         """Sends a link to the user that lets them donate to me."""        
@@ -112,5 +120,8 @@ if __name__ == '__main__':
     )
     
     crag.run(os.getenv('CRAGTOKEN'))
-    if crag.db.is_alive():
+    
+    try:
         asyncio.run(crag.db.close())
+    except ValueError:
+        pass
